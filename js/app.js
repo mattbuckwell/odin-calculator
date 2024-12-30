9efe1f958e23710c65a50e47aabfa9068b49c5e2
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-31 09:36:38 +1300

    Added variables and basic operation functions to app.js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// Global Variables for Project
+const firstNumber = 0;
+const secondNumber = 0;
+const operator = "";
+
+// Functions for calculator operation
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
