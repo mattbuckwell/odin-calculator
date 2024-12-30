2646b292cb074b701af1129a4c564c490978a913
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-31 09:52:27 +1300

    Added function to call the correct operation

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const firstNumber = 0;
 const secondNumber = 0;
 const operator = "";
 
-// Functions for calculator operation
+// Functions for calculator operations
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -18,4 +18,24 @@ function multiply (num1, num2) {
 
 function divide (num1, num2) {
     return num1 / num2;
+}
+
+// Function to call an operation
+function operate (num1, operator, num2) {
+    switch (operator) {
+        case "+" :
+            add(num1, num2);
+            break;
+        case "-" :
+            subtract(num1, num2);
+            break;
+        case "*" :
+            multiply(num1, num2);
+            break;
+        case "/" :
+            divide(num1, num2);
+            break;
+        default :
+            console.log("Not a valid operation");
+    }
 }
