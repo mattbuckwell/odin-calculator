4ad0445fffa7c8856789f27f19b554331b2d83f2
Matt Buckwell <mattbuckwell@gmail.com>
2024-12-31 10:20:29 +1300

    Added test cases to app.js to check functions

--- js/app.js
+++ js/app.js
@@ -24,18 +24,31 @@ function divide (num1, num2) {
 function operate (num1, operator, num2) {
     switch (operator) {
         case "+" :
-            add(num1, num2);
+            return add(num1, num2);
             break;
         case "-" :
-            subtract(num1, num2);
+            return subtract(num1, num2);
             break;
         case "*" :
-            multiply(num1, num2);
+            return multiply(num1, num2);
             break;
         case "/" :
-            divide(num1, num2);
+            return divide(num1, num2);
             break;
         default :
             console.log("Not a valid operation");
     }
 }
+
+// ----- Test cases -----
+console.log("-- Individual Functions --");
+console.log(`Addition of 2 + 2 = ${add(2,2)}`);
+console.log(`Subtraction of 10 - 3 = ${subtract(10, 3)}`);
+console.log(`Multiplication of 8 * 20 = ${multiply(8, 20)}`);
+console.log(`Division of 50 / 10 = ${divide(50, 10)}`);
+
+console.log("-- Main Function --");
+console.log(`Addition of 10 + 10 = ${operate(10, "+", 10)}`);
+console.log(`Subtraction of 1000 - 350 = ${operate(1000, "-", 350)}`);
+console.log(`Multiplication of 50 * 50 = ${operate(50, "*", 50)}`);
+console.log(`Division of 1000 / 10 = ${operate(1000, "/", 10)}`);
