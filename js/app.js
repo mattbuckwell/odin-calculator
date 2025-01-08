5406944e16ee8ae89f73cd95c2c984376688b193
Matt Buckwell <mattbuckwell@gmail.com>
2025-01-09 10:13:10 +1300

    Refactored app.js and removed unecessary code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-// ----- Global Variables for Project -----
-// let firstNumber = 0;
-// let secondNumber = 0;
-// let operator = "";
-// const reg = /[0-9+-/*.]/;
-
 // ----- HTML elements of the calculator -----
 const clearBtn = document.querySelector(".clear");
 const equalBtn = document.querySelector(".equal");
@@ -95,8 +89,8 @@ let calculator = {
         plusMinusBtn.disabled = false;
         dotBtn.disabled = false;
     },
-    operate (num1, operator, num2) {
-        switch (operator) {
+    evaluate (num1, num2) {
+        switch (this.operator) {
             case "+" :
                 return (num1 + num2).toFixed(2) / 1;;
             case "-" :
@@ -130,94 +124,13 @@ let calculator = {
         }
     
         calcDisplay.textContent = "";
-        calcDisplay.textContent = this.operate(this.firstNumber, this.operator, this.secondNumber);
+        calcDisplay.textContent = this.evaluate(this.firstNumber, this.secondNumber);
     },
 }
 
-// ----- Helper Functions -----
-// function percentConvert (value) {
-//     value = (parseFloat(value)) / 100;
-//     calcDisplay.textContent = value;
-//     percentBtn.disabled = true;
-// }
-
-// function clearCalc () {
-//     calcDisplay.textContent = "";
-//     firstNumber = 0;
-//     secondNumber = 0;
-//     operator = "";
-//     percentBtn.disabled = false;
-//     plusMinusBtn.disabled = false;
-//     dotBtn.disabled = false;
-// }
-
-// ----- Functions for calculator operations -----
-// function add (num1, num2) {
-//     return (num1 + num2).toFixed(2) / 1;
-// }
-
-// function subtract (num1, num2) {
-//     return (num1 - num2).toFixed(2) / 1;
-// }
-
-// function multiply (num1, num2) {
-//     return (num1 * num2).toFixed(2) / 1;
-// }
-
-// function divide (num1, num2) {
-//     return (num1 / num2).toFixed(2) / 1;
-// }
-
-// ----- Function to call an operation -----
-// function operate (num1, operator, num2) {
-//     switch (operator) {
-//         case "+" :
-//             return add(num1, num2);
-//         case "-" :
-//             return subtract(num1, num2);
-//         case "*" :
-//             return multiply(num1, num2);
-//         case "/" :
-//             if (num2 === 0) {
-//                 return "No dice!";
-//             }
-//             return divide(num1, num2);
-//         default :
-//             console.log("Not a valid operation");
-//     }
-// }
-
-// ----- Function for displaying calculator result -----
-// function result (equation) {
-//     let operatorPos = 0;
-//     for (let value in equation) {
-//         if (isNaN(equation[value]) && equation[value] !== ".") {
-//             operatorPos = parseInt(value);
-//             operator = equation[value];
-//         }
-//     }
-//     firstNumber = parseFloat(equation.slice(0, operatorPos));
-//     secondNumber = parseFloat(equation.slice(operatorPos + 1, equation.length));
-
-//     if (isNaN(firstNumber)) {
-//         firstNumber = 0;
-//     } else if (isNaN(secondNumber)) {
-//         secondNumber = 0;
-//     }
-
-//     calcDisplay.textContent = "";
-//     calcDisplay.textContent = operate(firstNumber, operator, secondNumber);
-// }
-
 // ----- Test cases -----
-// console.log("-- Individual Functions --");
-// console.log(`Addition of 2 + 2 = ${add(2,2)}`);
-// console.log(`Subtraction of 10 - 3 = ${subtract(10, 3)}`);
-// console.log(`Multiplication of 8 * 20 = ${multiply(8, 20)}`);
-// console.log(`Division of 50 / 10 = ${divide(50, 10)}`);
-
 console.log("-- Main Function --");
-console.log(`Addition of 10 + 10 = ${calculator.operate(10, "+", 10)}`);
-console.log(`Subtraction of 1000 - 350 = ${calculator.operate(1000, "-", 350)}`);
-console.log(`Multiplication of 50 * 50 = ${calculator.operate(50, "*", 50)}`);
-console.log(`Division of 1000 / 10 = ${calculator.operate(1000, "/", 10)}`);
+console.log(`Addition of 10 + 10 = ${calculator.evaluate(10, "+", 10)}`);
+console.log(`Subtraction of 1000 - 350 = ${calculator.evaluate(1000, "-", 350)}`);
+console.log(`Multiplication of 50 * 50 = ${calculator.evaluate(50, "*", 50)}`);
+console.log(`Division of 1000 / 10 = ${calculator.evaluate(1000, "/", 10)}`);
