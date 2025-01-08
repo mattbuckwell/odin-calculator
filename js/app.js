21a9ec45d7924a28446e43d299c6a4c0d3de4275
Matt Buckwell <mattbuckwell@gmail.com>
2025-01-09 09:47:31 +1300

    Added all functions and variables to a single object

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // ----- Global Variables for Project -----
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
-const reg = /[0-9+-/*.]/;
+// let firstNumber = 0;
+// let secondNumber = 0;
+// let operator = "";
+// const reg = /[0-9+-/*.]/;
 
 // ----- HTML elements of the calculator -----
 const clearBtn = document.querySelector(".clear");
@@ -23,7 +23,7 @@ calcBtn.forEach(element => {
 
 percentBtn.addEventListener("click", () => {
     if (calcDisplay.innerHTML !== "") {
-        percentConvert(calcDisplay.innerHTML);
+        calculator.percentConvert(calcDisplay.innerHTML);
     }
 });
 
@@ -50,115 +50,174 @@ dotBtn.addEventListener("click", () => {
 })
 
 clearBtn.addEventListener("click", () => {
-    clearCalc();
+    calculator.clearCalc();
 });
 
 equalBtn.addEventListener("click", () => {
-    result(calcDisplay.innerHTML);
+    calculator.result(calcDisplay.innerHTML);
     percentBtn.disabled = false;
     plusMinusBtn.disabled = false;
 })
 
 document.addEventListener("keydown", (e) => {
-    if (reg.test(e.key)) {
+    if (calculator.reg.test(e.key)) {
         calcDisplay.textContent += e.key;
     } else if (e.key === "Enter") {
         if (calcDisplay.innerHTML !== "") {
-            result(calcDisplay.innerHTML);
+            calculator.result(calcDisplay.innerHTML);
         }
     } else if (e.key === "c") {
-        clearCalc();
+        calculator.clearCalc();
     } else if (e.key === "%") {
         if (calcDisplay.innerHTML !== "") {
-            percentConvert(calcDisplay.innerHTML);
+            calculator.percentConvert(calcDisplay.innerHTML);
         }
     }
 })
 
-// ----- Helper Functions -----
-function percentConvert (value) {
-    value = (parseFloat(value)) / 100;
-    calcDisplay.textContent = value;
-    percentBtn.disabled = true;
+// ----- Calculator Object -----
+let calculator = {
+    firstNumber : 0,
+    secondNumber : 0,
+    operator : "",
+    reg : /[0-9+-/*.]/,
+    percentConvert (value) {
+        value = (parseFloat(value)) / 100;
+        calcDisplay.textContent = value;
+        percentBtn.disabled = true;
+    },
+    clearCalc () {
+        calcDisplay.textContent = "";
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+        this.operator = "";
+        percentBtn.disabled = false;
+        plusMinusBtn.disabled = false;
+        dotBtn.disabled = false;
+    },
+    operate (num1, operator, num2) {
+        switch (operator) {
+            case "+" :
+                return (num1 + num2).toFixed(2) / 1;;
+            case "-" :
+                return (num1 - num2).toFixed(2) / 1; 
+            case "*" :
+                return (num1 * num2).toFixed(2) / 1;
+            case "/" :
+                if (num2 === 0) {
+                    return "No dice!";
+                }
+                return (num1 / num2).toFixed(2) / 1;
+            default :
+                console.log("Not a valid operation");
+        }
+    },
+    result (equation) {
+        let operatorPos = 0;
+        for (let value in equation) {
+            if (isNaN(equation[value]) && equation[value] !== ".") {
+                operatorPos = parseInt(value);
+                this.operator = equation[value];
+            }
+        }
+        this.firstNumber = parseFloat(equation.slice(0, operatorPos));
+        this.secondNumber = parseFloat(equation.slice(operatorPos + 1, equation.length));
+    
+        if (isNaN(this.firstNumber)) {
+            this.firstNumber = 0;
+        } else if (isNaN(this.secondNumber)) {
+            this.secondNumber = 0;
+        }
+    
+        calcDisplay.textContent = "";
+        calcDisplay.textContent = this.operate(this.firstNumber, this.operator, this.secondNumber);
+    },
 }
 
-function clearCalc () {
-    calcDisplay.textContent = "";
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = "";
-    percentBtn.disabled = false;
-    plusMinusBtn.disabled = false;
-    dotBtn.disabled = false;
-}
+// ----- Helper Functions -----
+// function percentConvert (value) {
+//     value = (parseFloat(value)) / 100;
+//     calcDisplay.textContent = value;
+//     percentBtn.disabled = true;
+// }
+
+// function clearCalc () {
+//     calcDisplay.textContent = "";
+//     firstNumber = 0;
+//     secondNumber = 0;
+//     operator = "";
+//     percentBtn.disabled = false;
+//     plusMinusBtn.disabled = false;
+//     dotBtn.disabled = false;
+// }
 
 // ----- Functions for calculator operations -----
-function add (num1, num2) {
-    return (num1 + num2).toFixed(2) / 1;
-}
+// function add (num1, num2) {
+//     return (num1 + num2).toFixed(2) / 1;
+// }
 
-function subtract (num1, num2) {
-    return (num1 - num2).toFixed(2) / 1;
-}
+// function subtract (num1, num2) {
+//     return (num1 - num2).toFixed(2) / 1;
+// }
 
-function multiply (num1, num2) {
-    return (num1 * num2).toFixed(2) / 1;
-}
+// function multiply (num1, num2) {
+//     return (num1 * num2).toFixed(2) / 1;
+// }
 
-function divide (num1, num2) {
-    return (num1 / num2).toFixed(2) / 1;
-}
+// function divide (num1, num2) {
+//     return (num1 / num2).toFixed(2) / 1;
+// }
 
 // ----- Function to call an operation -----
-function operate (num1, operator, num2) {
-    switch (operator) {
-        case "+" :
-            return add(num1, num2);
-        case "-" :
-            return subtract(num1, num2);
-        case "*" :
-            return multiply(num1, num2);
-        case "/" :
-            if (num2 === 0) {
-                return "No dice!";
-            }
-            return divide(num1, num2);
-        default :
-            console.log("Not a valid operation");
-    }
-}
+// function operate (num1, operator, num2) {
+//     switch (operator) {
+//         case "+" :
+//             return add(num1, num2);
+//         case "-" :
+//             return subtract(num1, num2);
+//         case "*" :
+//             return multiply(num1, num2);
+//         case "/" :
+//             if (num2 === 0) {
+//                 return "No dice!";
+//             }
+//             return divide(num1, num2);
+//         default :
+//             console.log("Not a valid operation");
+//     }
+// }
 
 // ----- Function for displaying calculator result -----
-function result (equation) {
-    let operatorPos = 0;
-    for (let value in equation) {
-        if (isNaN(equation[value]) && equation[value] !== ".") {
-            operatorPos = parseInt(value);
-            operator = equation[value];
-        }
-    }
-    firstNumber = parseFloat(equation.slice(0, operatorPos));
-    secondNumber = parseFloat(equation.slice(operatorPos + 1, equation.length));
+// function result (equation) {
+//     let operatorPos = 0;
+//     for (let value in equation) {
+//         if (isNaN(equation[value]) && equation[value] !== ".") {
+//             operatorPos = parseInt(value);
+//             operator = equation[value];
+//         }
+//     }
+//     firstNumber = parseFloat(equation.slice(0, operatorPos));
+//     secondNumber = parseFloat(equation.slice(operatorPos + 1, equation.length));
 
-    if (isNaN(firstNumber)) {
-        firstNumber = 0;
-    } else if (isNaN(secondNumber)) {
-        secondNumber = 0;
-    }
+//     if (isNaN(firstNumber)) {
+//         firstNumber = 0;
+//     } else if (isNaN(secondNumber)) {
+//         secondNumber = 0;
+//     }
 
-    calcDisplay.textContent = "";
-    calcDisplay.textContent = operate(firstNumber, operator, secondNumber);
-}
+//     calcDisplay.textContent = "";
+//     calcDisplay.textContent = operate(firstNumber, operator, secondNumber);
+// }
 
 // ----- Test cases -----
-console.log("-- Individual Functions --");
-console.log(`Addition of 2 + 2 = ${add(2,2)}`);
-console.log(`Subtraction of 10 - 3 = ${subtract(10, 3)}`);
-console.log(`Multiplication of 8 * 20 = ${multiply(8, 20)}`);
-console.log(`Division of 50 / 10 = ${divide(50, 10)}`);
+// console.log("-- Individual Functions --");
+// console.log(`Addition of 2 + 2 = ${add(2,2)}`);
+// console.log(`Subtraction of 10 - 3 = ${subtract(10, 3)}`);
+// console.log(`Multiplication of 8 * 20 = ${multiply(8, 20)}`);
+// console.log(`Division of 50 / 10 = ${divide(50, 10)}`);
 
 console.log("-- Main Function --");
-console.log(`Addition of 10 + 10 = ${operate(10, "+", 10)}`);
-console.log(`Subtraction of 1000 - 350 = ${operate(1000, "-", 350)}`);
-console.log(`Multiplication of 50 * 50 = ${operate(50, "*", 50)}`);
-console.log(`Division of 1000 / 10 = ${operate(1000, "/", 10)}`);
+console.log(`Addition of 10 + 10 = ${calculator.operate(10, "+", 10)}`);
+console.log(`Subtraction of 1000 - 350 = ${calculator.operate(1000, "-", 350)}`);
+console.log(`Multiplication of 50 * 50 = ${calculator.operate(50, "*", 50)}`);
+console.log(`Division of 1000 / 10 = ${calculator.operate(1000, "/", 10)}`);
